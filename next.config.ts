5f74362d4ca82b4bdb83642fbe30c615d958a6fe
KamoteNga <kamoteka143din@gmail.com>
2025-11-19 22:35:51 +0800

    address error on the build

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbopack: {
-      root: ".",
-      resolveExtensions: [
-        ".mdx",
-        ".tsx",
-        ".ts",
-        ".jsx",
-        ".js",
-        ".mjs",
-        ".json",
-      ],
-      resolveAlias: {
-        // You can add aliases here if needed
-      },
-    },
-  },
 };
 
 export default nextConfig;
